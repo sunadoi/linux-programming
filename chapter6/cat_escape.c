8e12250aef7f57ce9daea70ccd976a9762a3878d
sunadoi <y.sunadoi@gmail.com>
2021-09-25 00:16:48 +0900

    cat_escape

---
+++ chapter6/cat_escape.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void do_cat(FILE *f);
+
+int
+main(int argc, char *argv[])
+{
+  int i;
+  if (argc == 1) {
+    do_cat(stdin);
+    exit(0);
+  }
+
+  for (i = 1; i < argc; i++) {
+    FILE *f;
+
+    // 返り値はfile descripter
+    f = fopen(argv[i], "r");
+    if (!f) {
+      perror(argv[i]);
+      exit(1);
+    }
+    do_cat(f);
+    fclose(f);
+  }
+  exit(0);
+}
+
+static void
+do_cat(FILE *f)
+{
+  int c;
+
+  // file descripterから読み込む。EOFが最後なので最後まで読み込むループ
+  while((c = fgetc(f)) != EOF) {
+    // putchar(c) = putc(c, stdout)
+    switch (c) {
+      case '\t':
+        if (fputs("\\t", stdout) == EOF) exit(1);
+        break;
+      case '\n':
+        if (fputs("$\n", stdout) == EOF) exit(1);
+        break;
+      default:
+        if (putchar(c) < 0) exit(1);
+        break;
+    }
+  }
+}
