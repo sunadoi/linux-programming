fd86a125bc5b5747688ea6b3f0398f67e7b71cbb
sunadoi <y.sunadoi@gmail.com>
2021-09-25 00:02:14 +0900

    fix: stdio_cat

--- chapter6/stdio_cat.c
+++ chapter6/stdio_cat.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void do_cat(FILE *f);
+
 int
 main(int argc, char *argv[])
 {
   int i;
+  if (argc == 1) {
+    do_cat(stdin);
+    exit(0);
+  }
+
   for (i = 1; i < argc; i++) {
     FILE *f;
-    int c;
 
     // 返り値はfile descripter
     f = fopen(argv[i], "r");
@@ -15,12 +21,20 @@ main(int argc, char *argv[])
       perror(argv[i]);
       exit(1);
     }
-    // file descripterから読み込む。EOFが最後なので最後まで読み込むループ
-    while((c = fgetc(f)) != EOF) {
-      // putchar(c) = putc(c, stdout)
-      if (putchar(c) < 0) exit(1);
-    }
+    do_cat(f);
     fclose(f);
   }
   exit(0);
+}
+
+static void
+do_cat(FILE *f)
+{
+  int c;
+
+  // file descripterから読み込む。EOFが最後なので最後まで読み込むループ
+  while((c = fgetc(f)) != EOF) {
+    // putchar(c) = putc(c, stdout)
+    if (putchar(c) < 0) exit(1);
+  }
 }
